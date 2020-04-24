4acb375687066c66c67cd56e846733e43a3cac17
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-24 11:52:29 +0530

    square servlet added

---
+++ Exp11.3/Square.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Square extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		
+		int k = (int)req.getAttribute("k");
+		
+		k = k * k;
+		
+		PrintWriter out = res.getWriter();
+		out.println("Hello");
+		out.println("Result is : " + k);
+	}
+}
